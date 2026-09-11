c5ca258f8b0515836789479a7e90d174cf347bb8
Adam Krawiec <adam@gmail.com>
2026-09-11 14:30:00 +0200

    Zad.3

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,9 +1,14 @@
 void main() {
+    //1
     System.out.println("Ania");
     System.out.println("Bartek");
     System.out.println("Kasia");
 
+    //2
     String imie = "Adam";
     int rokUrodzenia = 2007;
     double liczba = 0.66;
+
+    //3
+    System.out.println("Mam na imię "+imie+", mam "+(2026-rokUrodzenia)+" lat  i będę pisać maturę za "+liczba+" roku.");
 }
