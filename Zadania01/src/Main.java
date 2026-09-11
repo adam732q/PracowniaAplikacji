5f405e33464b327a920c672334b5ed8534790751
Adam Krawiec <adam@gmail.com>
2026-09-11 14:36:39 +0200

    Zad.4

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -11,4 +11,12 @@ void main() {
 
     //3
     System.out.println("Mam na imię "+imie+", mam "+(2026-rokUrodzenia)+" lat  i będę pisać maturę za "+liczba+" roku.");
+
+    //4
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Podaj temperaturę na zewnątrz: ");
+    double stopnie =sc.nextDouble();
+    double farenheit = 1.8 * stopnie + 32.0;
+    System.out.println("Temperatura w farenheit: " + farenheit);
 }
