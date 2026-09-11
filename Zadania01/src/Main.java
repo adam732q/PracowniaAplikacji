08b658a4e830b67253c9ca978b667a80e1016915
Adam Krawiec <adam@gmail.com>
2026-09-11 14:27:36 +0200

    Zad.2

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -2,4 +2,8 @@ void main() {
     System.out.println("Ania");
     System.out.println("Bartek");
     System.out.println("Kasia");
+
+    String imie = "Adam";
+    int rokUrodzenia = 2007;
+    double liczba = 0.66;
 }
