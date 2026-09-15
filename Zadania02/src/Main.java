323b33d464a14ce6c68fabd63202bb5a619a31b8
Adam Krawiec <adam@gmail.com>
2026-09-15 08:39:46 +0200

    Zadanie 1

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -1,13 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    //1
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Podaj liczbe: ");
+    int liczba = sc.nextInt();
+    if(liczba%3==0){
+        System.out.println("Twoja Liczba jest podzielna przez 3");
+    }else{
+        System.out.println("Twoja liczba nie jest podzielna przez 3");
     }
 }
