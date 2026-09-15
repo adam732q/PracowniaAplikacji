1ba7e96147e9a36aa70cb82669d17c0683eddacd
Adam Krawiec <adam@gmail.com>
2026-09-15 08:50:09 +0200

    Zadanie 2

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -8,4 +8,22 @@ void main() {
     }else{
         System.out.println("Twoja liczba nie jest podzielna przez 3");
     }
+
+    //2
+    Scanner sc2 = new Scanner(System.in);
+
+    System.out.print("Podaj 1 bok trójkąta: ");
+    int bok1 = sc2.nextInt();
+
+    System.out.print("Podaj 2 bok trójkąta: ");
+    int bok2 = sc2.nextInt();
+
+    System.out.print("Podaj 3 bok trójkąta: ");
+    int bok3 = sc2.nextInt();
+
+    if(bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1){
+        System.out.println("Z twoich liczb można zbudować trójkąt");
+    }else{
+        System.out.println("Z twoich liczb niemożna zbudować trójkąta");
+    }
 }
