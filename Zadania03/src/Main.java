4348a0b02aea9178e0fd45d1b5c3f643cbae0744
Adam <adam@gmail.com>
2026-09-21 19:40:40 +0200

    zad 2

--- Zadania03/src/Main.java
+++ Zadania03/src/Main.java
@@ -8,4 +8,15 @@ void main() {
     for(int i = 1; i <= liczba; i += 2){
         System.out.println(i);
     }
+
+    //2
+    System.out.print("Podaj liczbę: ");
+    int n = sc.nextInt();
+
+    int potega = 1;
+
+    while(potega <= n){
+        System.out.println(potega);
+        potega = potega * 2;
+    }
 }
